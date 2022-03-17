f0da29dced61c807ea4439c5a3d4304b22f44b56
IgnacioChalub <ignaciochalub@gmail.com>
2022-03-17 19:33:08 -0300

    minor corretion

--- src/infrastructure/persistance/callerDAO.ts
+++ src/infrastructure/persistance/callerDAO.ts
@@ -3,6 +3,9 @@ import Caller from "../../domain/entities/caller";
 
 class CallerDAO implements ICallerRepository{
     create(caller: Caller): void {
-
+    }
+    getByDNIorEmail(DNI: number, email: string): Caller {
+        // @ts-ignore
+        return undefined;
     }
 }
