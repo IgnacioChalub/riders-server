710de703dbfe1d490896c0aeea0f36c5c6d1ec87
IgnacioChalub <ignaciochalub@gmail.com>
2022-03-31 10:13:37 -0300

    nfsjda

--- src/domain/services/caller/createCall.domainService.ts
+++ src/domain/services/caller/createCall.domainService.ts
@@ -6,17 +6,9 @@ import RequestedVehicles from "../../entities/requestedVehicles";
 class CreateCallDomainService{
 
     run(id: string, caller: Caller, requestedVehicles: RequestedVehicles, priceInCents: number, description: string, startLocation: Location, finishLocation: Location): Call{
-        if(priceInCents < 0) throw Error("Price can not below 0");
-        //if(!this.validateVehicles(vehicleTypes)) throw Error('Invalid vehicle type');
+        if(priceInCents < 0) throw Error("Price can not be under 0");
         return new Call(id, caller.getId(), caller.getRatingStart(), requestedVehicles, priceInCents, description, startLocation, finishLocation, true);
     }
-
-    // validateVehicles(vehicleTypes: string[]): boolean{
-    //     for (const type of vehicleTypes) {
-    //        if(!(Object.values(VehicleTypes).includes(type as VehicleTypes))) return false;
-    //     }
-    //     return true;
-    // }
 }
 
 export default CreateCallDomainService;
