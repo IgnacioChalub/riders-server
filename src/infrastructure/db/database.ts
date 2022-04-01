a7895ae5d97c6e0d3bf64ae35861988cb0100254
IgnacioChalub <ignaciochalub@gmail.com>
2022-04-01 13:52:18 -0300

    change postgres docker port

--- src/infrastructure/db/database.ts
+++ src/infrastructure/db/database.ts
@@ -5,13 +5,14 @@ import Call from "../../domain/entities/call";
 import 'dotenv/config'
 
 const runDbConnection = async (): Promise<void> => {
+    const port: number = <number><unknown>process.env.DB_PORT;
     const connectionManager = getConnectionManager();
     const connection = connectionManager.create({
         // connection name
         name: "db",
         type: "postgres",
         host: process.env.DB_HOST,
-        port: 5432,
+        port: port,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
