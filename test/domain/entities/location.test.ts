fe97863479250704f882a8740cf5c0660ebf9180
IgnacioChalub <ignaciochalub@gmail.com>
2022-04-19 13:30:32 -0300

    added test cases

--- test/domain/entities/location.test.ts
+++ test/domain/entities/location.test.ts
@@ -3,13 +3,15 @@ import {Location} from "../../../src/domain/entities/location";
 test('test location is in radius', async() => {
     const startLocation: Location = Location.create("1","address",-34.411244, -58.663669);
     const coordsInRadius = [
+        [-34.411244, -58.663669],
         [-34.4068654,-58.6578094],
         [-34.4131326,-58.6438508],
         [-34.441822, -58.579475],
         [-34.449111, -58.571548],
-        [-34.420216, -58.769847]
+        [-34.420216, -58.769847],
     ]
     const coordsNotInRadius = [
+        [34.411244, 58.663669],
         [-34.321244, -58.663669],
         [-34.420352, -58.775656]
     ]
