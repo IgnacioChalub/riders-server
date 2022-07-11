f27410a29872737747ee8b2afb20cf62effcc91f
IgnacioChalub <ignaciochalub@gmail.com>
2022-07-11 09:38:47 -0300

    cors in sockets

--- src/infrastructure/socket/socketServer.ts
+++ src/infrastructure/socket/socketServer.ts
@@ -5,7 +5,15 @@ import {SocketManager} from "./socketManager";
 
 
 const server = http.createServer(app);
-const io = new WebSocketServer(server);
+//const io = new WebSocketServer(server);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        allowHeaders: ['auth-token', 'user-type'],
+        credentials: true
+    }
+})
 
 SocketManager.create(io);
 
